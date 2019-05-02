ed208be799aadc2d8ab6c13e7ff46710dd132c5c
David Hew-wing <dhew67@gmail.com>
2019-05-02 15:06:43 -0400

    added nav drawer

--- app/src/main/java/com/example/wordsearch/MainActivity.kt
+++ app/src/main/java/com/example/wordsearch/MainActivity.kt
@@ -1,46 +1,75 @@
 package com.example.wordsearch
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.TabLayout
-import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentManager
-import android.support.v4.app.FragmentPagerAdapter
-import android.support.v4.view.ViewPager
+import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.MenuItem
+import android.support.v4.widget.DrawerLayout
+import android.support.design.widget.NavigationView
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
+import android.view.Menu
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    private lateinit var viewPager: ViewPager
-    private lateinit var tabs: TabLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
-        initViews()
-        setupViewPager()
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navView.setNavigationItemSelectedListener(this)
     }
 
-    private fun initViews() {
-        tabs = findViewById(R.id.tabs)
-        viewPager = findViewById(R.id.viewpager)
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
     }
 
-    private fun setupViewPager() {
-
-        val adapter = MyFragmentPagerAdapter(getSupportFragmentManager())
-
-        var firstFragmet: MyFragment = MyFragment.newInstance("First Fragment")
-        var secondFragmet: MyFragment = MyFragment.newInstance("Second Fragment")
-        var thirdFragmet: MyFragment = MyFragment.newInstance("Third Fragment")
-
-        adapter.addFragment(firstFragmet, "ONE")
-        adapter.addFragment(secondFragmet, "TWO")
-        adapter.addFragment(thirdFragmet, "THREE")
-
-        viewPager!!.adapter = adapter
-
-        tabs!!.setupWithViewPager(viewPager)
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        return false
     }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_play -> {
+                // Handle the camera action
+            }
+            R.id.nav_settings-> {
+
+            }
+            R.id.nav_aboutme -> {
+
+            }
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
 }
